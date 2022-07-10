776a928b663e25c3d68b053891c5bc063b2f32b1
hemersonrafael <hemerson@geowellex.com>
2022-07-10 07:30:21 -0300

    Adding LICENSE.

--- witsml_consumer_api/main.py
+++ witsml_consumer_api/main.py
@@ -7,8 +7,29 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from witsml_consumer_api.routers import logs, wellbores, wells
+description="""
+This RESTFull API consumes data from a WITSML storage.
 
-app = FastAPI()
+Supported WITSML Data version: 1.4.1.1
+
+Supported WITSML data objects based on Standards maintained by [Energistics](https://www.energistics.org/witsml-data-standards/) standards:
+- Well
+- Wellbore
+- Log
+"""
+app = FastAPI(
+    title="WITSML CONSUMER API",
+    description=description,
+    version="0.1.0",
+    contact={
+        "name": "Hemerson Rafael",
+        "email": "rafaelpontes1995@gmail.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
 
 
 app.add_middleware(
