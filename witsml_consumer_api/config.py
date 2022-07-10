312734ac3d12ab9f9846f4f5fdc11b62019be875
hemersonrafael <hemerson@geowellex.com>
2022-07-10 07:35:04 -0300

    Adding OpenAPI Settings.

--- witsml_consumer_api/config.py
+++ witsml_consumer_api/config.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
 """Configurations."""
-from pydantic import BaseModel, BaseSettings
-
-# class DatabaseConfig (BaseModel):
-#     DIALECT: str = "postgresql"
-#     DRIVER: str = "psycopg2"
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-
-    # DATABASE_CONFIG : DatabaseConfig = DatabaseConfig()
-    # POSTGRES_HOST: str
-    # POSTGRES_PORT: str
-    # POSTGRES_USER: str
-    # POSTGRES_PASS: str
-    # POSTGRES_DB: str
     WITSML_SERVICE_URL: str
     WITSML_USERNAME: str
     WITSML_PASSWORD: str
@@ -25,3 +14,78 @@ class Settings(BaseSettings):
 
 
 settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
+
+description: str = """
+This RESTFull API consumes data from a WITSML storage.
+
+Supported WITSML Data version: 1.4.1.1
+
+Supported WITSML data objects based on Standards maintained by [Energistics](https://www.energistics.org/witsml-data-standards/):
+- Log
+- Well
+- Wellbore
+
+Don't forget to set the environment variables:
+* **WITSML_SERVICE_URL** -> The WITSML storage URL. will try to connect.
+* **WITSML_USERNAME** -> The WITSML username used by [komle-plus](https://github.com/HemersonRafael/komle-plus).
+* **WITSML_PASSWORD** -> The WITSML password to be set on the user above.
+
+**Attention with Security**
+
+Before deploying in production is necessary choice one way among many 
+ways to handle security, authentication and authorization.
+
+Some possible options:
+- [OAuth2](https://oauth.net/2/)
+- [OAuth 1](https://oauth.net/1/)
+- [OpenID Connect](https://openid.net/connect/)
+
+[More](https://github.com/HemersonRafael/witsml-consumer-api)
+"""
+
+tags_metadata: list = [
+    {
+        'name': 'active',
+        'description': 'To check if the API is active.',
+    },
+    {
+        'name': 'logs',
+        'description': 'The log object is used to capture the curves on a well log.',
+        'externalDocs': {
+            'description': 'Logs external docs',
+            'url': 'http://w3.energistics.org/schema/WITSML_v1.4.1.1_Data_Schema_with_Raster_v1.0/witsml_v1.4.1.1_data/doc/witsml_schema_overview.html#_log_schema',
+        },
+    },
+    {
+        'name': 'wells',
+        'description': 'The well object is used to capture the general information about a well.',
+        'externalDocs': {
+            'description': 'Wells external docs',
+            'url': 'http://w3.energistics.org/schema/WITSML_v1.4.1.1_Data_Schema_with_Raster_v1.0/witsml_v1.4.1.1_data/doc/witsml_schema_overview.html#_well_schema',
+        },
+    },
+    {
+        'name': 'wellbores',
+        'description': 'The wellbore object is used to capture the general information about a wellbore.',
+        'externalDocs': {
+            'description': 'Wellbores external docs',
+            'url': 'http://w3.energistics.org/schema/WITSML_v1.4.1.1_Data_Schema_with_Raster_v1.0/witsml_v1.4.1.1_data/doc/witsml_schema_overview.html#_wellbore_schema',
+        },
+    },
+]
+
+open_api_settings = {
+    'title': 'WITSML CONSUMER API',
+    'version': '0.1.0',
+    'description': description,
+    'contact': {
+        'name': 'Hemerson Rafael',
+        'email': 'rafaelpontes1995@gmail.com',
+        'url': 'https://www.linkedin.com/in/hemerson-rafael/',
+    },
+    'license_info': {
+        'name': 'Apache 2.0',
+        'url': 'https://www.apache.org/licenses/LICENSE-2.0.html',
+    },
+    'openapi_tags': tags_metadata,
+}
--- witsml_consumer_api/main.py
+++ witsml_consumer_api/main.py
@@ -2,35 +2,13 @@
 """Main."""
 from typing import Any
 
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from witsml_consumer_api.config import open_api_settings
 from witsml_consumer_api.routers import logs, wellbores, wells
-description="""
-This RESTFull API consumes data from a WITSML storage.
 
-Supported WITSML Data version: 1.4.1.1
-
-Supported WITSML data objects based on Standards maintained by [Energistics](https://www.energistics.org/witsml-data-standards/) standards:
-- Well
-- Wellbore
-- Log
-"""
-app = FastAPI(
-    title="WITSML CONSUMER API",
-    description=description,
-    version="0.1.0",
-    contact={
-        "name": "Hemerson Rafael",
-        "email": "rafaelpontes1995@gmail.com",
-    },
-    license_info={
-        "name": "Apache 2.0",
-        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-    },
-)
-
+app = FastAPI(**open_api_settings)
 
 app.add_middleware(
     CORSMiddleware,
@@ -40,21 +18,11 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-app.include_router(logs.router)
-app.include_router(wells.router)
-app.include_router(wellbores.router)
+app.include_router(logs.router, tags=['logs'])
+app.include_router(wells.router, tags=['wells'])
+app.include_router(wellbores.router, tags=['wellbores'])
 
 
-@app.get('/')
+@app.get('/', tags=['active'])
 async def root() -> Any:
     return {'message': 'I am active!'}
-
-
-if __name__ == '__main__':
-    uvicorn.run(
-        app='main:app',
-        host='0.0.0.0',
-        port=8080,
-        log_level='debug',
-        reload=True,
-    )
