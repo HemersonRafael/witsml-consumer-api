88cfc631584c24d7876a7d79f46fd69ae5b40860
hemersonrafael <hemerson@geowellex.com>
2022-07-10 07:37:44 -0300

    Changing  json -> ujson.

--- witsml_consumer_api/crud/base.py
+++ witsml_consumer_api/crud/base.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """Implementation of the base CRUD."""
-import json
+import ujson
 
 
 class CRUDBase:
     def clean_obj(self, obj: dict) -> dict:
-        return json.loads(
-            json.dumps(obj)
+        return ujson.loads(
+            ujson.dumps(obj)
             .replace('"ns1:', '"')
             .replace('"@', '"')
             .replace(':ns1"', '"')
